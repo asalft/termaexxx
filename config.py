0b72f8786e24b857ea03375d9d979aed3c58d7d1
آسال <telebotimport@gmail.com>
2025-10-01 18:22:20 +0300

    Uploaded Successfully

---
+++ config.py
@@ -0,0 +1,16 @@
+
+from asBASE import asJSON
+db = asJSON("as.json")
+
+SUDORS = [6583786208]
+
+
+
+class Config:
+    API_ID = 8186557
+    API_HASH = "efd77b34c69c164ce158037ff5a0d117"
+    TOKEN = "8189272424:AAGuJhFSK_TAO7PLTtsJZDyj8yYAXw1WzWk"  
+    START_PIC = "https://graph.org/file/864483e9fb1cec38b67fe.jpg" 
+    CHAT = "VeGaOne" 
+   
+    bot_id = TOKEN.split(":")[0]    
